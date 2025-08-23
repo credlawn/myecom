04fe32c5c8c78f168896bf7239700d48da4eea08
Arun <home@Aruns-MacBook-Air.local>
2025-08-23 16:29:14 +0530

    New Changes - 23 Aug 04:29 PM

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "paisamilega.in",
+        hostname: "credlawn.com",
         pathname: "/files/**",
       },
     ],
