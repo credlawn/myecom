b41bd10a958e2c8365d2c6e44b6f492598c6e3e3
Arun <home@Aruns-MacBook-Air.local>
2025-08-30 01:21:42 +0530

    New Changes - 30 Aug 01:21 am

--- frontend/src/app/products/[slug]/ProductContent.tsx
+++ frontend/src/app/products/[slug]/ProductContent.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { SingleProduct } from "@/types/product";
+import { SingleProduct } from "@/models/productData";
 import ProductGallery from "./ProductGallery";
 
 export default function ProductContent({
