43a10d7ceb4992633d61be56857abf8ed04a49bd
Arun Singh <admin@credlawn.com>
2025-09-01 13:02:53 +0530

    bug fix

--- frontend/src/app/products/[slug]/ProductGallery.tsx
+++ frontend/src/app/products/[slug]/ProductGallery.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import ImageModal from "./ImageModal";
-import { SingleProduct } from "@/models/productData";
+import { SingleProduct } from "@/myapi/productData";
 
 import "swiper/css";
 import "swiper/css/pagination";
